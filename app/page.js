f4c119c49459dc8724f4878136c0590973926f3d
akunsamtiga <akunsamtiga@gmail.com>
2025-05-20 09:04:04 +0700

    f

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Art from "./components/Art";
 import Art2 from "./components/Art2";
 import CaraOrder from "./components/CaraOrder";
@@ -6,12 +7,21 @@ import Rooms from "./components/Rooms";
 
 export default function Home() {
   return (
-    <main className="bg-gray-100">
-      <Hero />
-      <Rooms />
-      <Art />
-      <Art2 />
-      <CaraOrder />
-    </main>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="hU3YoPIg9qaWGhByXfXk5ynyriLME_4rIdojZCQBKck"
+        />
+      </Head>
+
+      <main className="bg-gray-100">
+        <Hero />
+        <Rooms />
+        <Art />
+        <Art2 />
+        <CaraOrder />
+      </main>
+    </>
   );
 }
